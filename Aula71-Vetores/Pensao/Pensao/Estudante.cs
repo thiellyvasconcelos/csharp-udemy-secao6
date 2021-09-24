506c7838540a568a9af96daa5ba324f0e7fe03fc
Thielly <thiellyvr@gmail.com>
2021-09-24 16:00:13 -0300

    Este exercicio contem a resolucao de alocacao de quartos utilizando vetores

---
+++ Aula71-Vetores/Pensao/Pensao/Estudante.cs
@@ -0,0 +1,20 @@
+﻿namespace Pensao
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, E-mail: {Email}"; 
+        }
+    }
+}
---
+++ Aula71-Vetores/Pensao/Pensao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vetor = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= qnt; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for(int i = 1; i < 10; i++)
+            {
+                if(vetor[i] != null) 
+                {
+                    Console.WriteLine($"Quarto: {i}, {vetor[i]}");
+                }
+            }
+        }
+    }
+}
