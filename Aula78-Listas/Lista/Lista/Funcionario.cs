b7c62a2707a2ed31e7ecf7de0081a04c78811684
Thielly <thiellyvr@gmail.com>
2021-09-28 00:00:48 -0300

    Programa cuja funcao pede a quantidade de funcionarios e adiciona suas informacoes em uma Lista, depois verifica com o usuario qual funcionario deseja aumentar o salario e a porcentagem pelo numero de Id do funcionario (usa-se expressao Lambda para validar o Id verificando na Lista)

---
+++ Aula78-Listas/Lista/Lista/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lista
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncrementarSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Nome: {Nome}, Salário: R${Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Aula78-Listas/Lista/Lista/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de funcionários: ");
+            int qnt = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Funcionario> list = new List<Funcionario>();
+            
+            for (int i = 1; i <= qnt; i++)
+            {
+                Console.WriteLine($"Funcionário {i}");
+                Console.Write($"Id do funcionário: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write($"Nome do funcionário: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário atual do funcionário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Digite o Id do funcionário que deseja aumentar o salário: ");
+            int buscaId = int.Parse(Console.ReadLine());
+
+            // Expressão Lambda, cujo vai identificar na lista se o Id informado anteriormente existe
+            Funcionario func = list.Find(x => x.Id == buscaId);
+            if( func != null)
+            {
+                Console.Write("Digite a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.IncrementarSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("O Id informado não existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista de funcionários atualizada - ");
+            foreach(Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
